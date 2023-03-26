bb506ca85007ae36d0af04374ec407053bff7cfa
PGw67232 <w67232@student.wsiz.edu.pl>
2023-03-26 18:24:09 +0200

    commit

--- lab1/4.py
+++ lab1/4.py
@@ -1,8 +1,7 @@
-
-tab=[-3,-4,-9,0,1,3,15,90,9,17,-1]
-naj=tab[0]
+tab = [-3, -4, -9, 0, 1, 3, 15, 90, 9, 17, -1]
+naj = tab[0]
 for x in tab:
-    if x<=naj:
-        naj=tab[x]
+    if x <= naj:
+        naj = tab[x]
 
 print(f"najmniejsza liczba w liście to : {naj}")
--- lab1/5.py
+++ lab1/5.py
@@ -1,9 +1,8 @@
 tab2d = [[1, 2, 3], [4, 5, 6], [-1, -2, -3], [-4, -5, -6]]
 print(tab2d)
 for w in tab2d:
-    minw = min(w)  
-    mini = w.index(minw)  
-    w[0], w[mini] = w[mini], w[0]  
+    minw = min(w)
+    mini = w.index(minw)
+    w[0], w[mini] = w[mini], w[0]
 
 print(tab2d)
-        
